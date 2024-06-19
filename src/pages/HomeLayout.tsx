5c62fbf6cf754897f4ba3087254eae975aea0919
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-19 17:53:39 +0530

    custom tailwind class implemented

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
-    <>
+    <div className='align-element py-20'>
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 };
 export default HomeLayout;
