47168077613010decb4cd744b45e79e66fc5eb8a
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-20 11:50:00 +0530

    landing page components created

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,11 @@
+import { Hero, FeaturedProducts } from '@/components';
+
 function Landing() {
-    return <h1 className="text-4xl">Landing Page</h1>;
-  }
-  export default Landing;
-  
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
+}
+export default Landing;
