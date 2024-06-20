d2df00ba9f934957862192a880dc06a229c7c5a1
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-20 16:19:11 +0530

    Loading component with shadcn and useNavigation state to load

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,13 +1,21 @@
 import { Header, Navbar } from "@/components";
-import { Outlet } from "react-router-dom";
+import Loading from "@/components/Loading";
+import { Outlet, useNavigation } from "react-router-dom";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  console.log("navigation", navigation);
+
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div className='align-element py-20'>
+    <>
       <Header />
       <Navbar />
-      <Outlet />
-    </div>
+      <div className="align-element py-20">
+        {isLoading ? <Loading /> : <Outlet />}
+      </div>
+    </>
   );
 };
 export default HomeLayout;
