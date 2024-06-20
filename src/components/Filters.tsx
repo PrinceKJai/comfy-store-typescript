00fc4291e769be74280b25dc22705f114c33c94d
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-20 18:59:55 +0530

    FormInput component created and used in filters comp

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,22 +1,19 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "./ui/button";
 import { ProductsResponseWithParams } from "@/utils";
+import FormInput from "./FormInput";
 
 function Filters() {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
-  const { search, category } = params;
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-      <div className="mb-2">
-        <Label htmlFor="search">Search Product</Label>
-        <Input id="search" name="search" type="text" defaultValue={search} />
-      </div>
-      <div className="mb-2">
-        <Label htmlFor="category">Search Category</Label>
-        <Input id="category" name="category" type="text" defaultValue={category} />
-      </div>
+      <FormInput
+        type="text"
+        name="search"
+        defaultValue={search}
+        label="Search Product"
+      />
       <Button type="submit" size="sm" className="self-end mb-2">
         search
       </Button>
