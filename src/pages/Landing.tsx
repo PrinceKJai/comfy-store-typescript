f880c1b73946536adf6a0d89d93332e0b6046598
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-20 12:42:18 +0530

    making api calls using LoaderFunction, useLoaderData from r-r-d

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,18 @@
 import { Hero, FeaturedProducts } from '@/components';
+import { customFetch } from '@/utils/customFetch';
+import { ProductsResponse } from '@/utils/types';
+import { LoaderFunction, useLoaderData } from 'react-router-dom';
+
+const url = '/products?featured=true';
+
+export const loader: LoaderFunction = async () : Promise<ProductsResponse> => {
+  const response = await customFetch.get<ProductsResponse>(url);
+  return { ...response.data };
+}
 
 function Landing() {
+  const data = useLoaderData() as ProductsResponse;
+  console.log("data", data);
   return (
     <>
       <Hero />
