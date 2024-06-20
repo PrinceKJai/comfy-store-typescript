a889e0bcbab9621bf578b99d3505c6d7efb93d78
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-20 10:59:02 +0530

    ErrorComponent created using useRouteError n isRouteErrorResponse

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
     {
       path: "/",
       element: <HomeLayout />,
+      errorElement: <Error />,
       children: [
         {
           index: true,
@@ -49,10 +50,12 @@ function App() {
     {
       path: "/login",
       element: <Login />,
+      errorElement: <Error />,
     },
     {
       path: "/register",
       element: <Register />,
+      errorElement: <Error />,
     },
   ]);
   return <RouterProvider router={router} />;
