9bbf61937c1e18bf870d2cc0c18f48e196f7dc02
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-21 18:45:55 +0530

    toast notification setup and cartSlice initial creation w/o functionality

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,12 +1,32 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CartState } from "@/utils";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
-  name: 'cart slice',
+const defaultState: CartState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+const getCartFromLocalStorage = (): CartState => {
+  const cart = localStorage.getItem("cart");
+  return cart ? JSON.parse(cart) : defaultState;
 };
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {},
+  name: "cart",
+  initialState: getCartFromLocalStorage(),
+  reducers: {
+    addItem: () => {},
+    editItem: () => {},
+    removeItem: () => {},
+    clearCart: () => {},
+    calculateTotals: () => {},
+  },
 });
+
+export const { addItem, editItem, removeItem, clearCart, calculateTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -50,4 +50,26 @@ export type SingleProductResponse = {
 };
 
 export type ProductsResponseWithParams = ProductsResponse & { params: Params };
-export type SingleProductResponseWithParams = SingleProductResponse & { params: Params };
+export type SingleProductResponseWithParams = SingleProductResponse & {
+  params: Params;
+};
+
+export type CartItem = {
+  cartID: string;
+  productID: number;
+  image: string;
+  title: string;
+  price: string;
+  amount: number;
+  productColor: string;
+  company: string;
+};
+
+export type CartState = {
+  cartItems: CartItem[];
+  numItemsInCart: number;
+  cartTotal: number;
+  shipping: number;
+  tax: number;
+  orderTotal: number;
+};
